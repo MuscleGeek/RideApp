aed68b8035d84bd4a55e062db4811684f68cb08b
Jonathan <hackertristania@gmail.com>
2021-11-23 02:04:11 -0600

    fixing minor issues

--- db.js
+++ db.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose")
 
 require('dotenv').config()
 
-const CONNECTION_STRINGS = process.env.CONNECTION_STRINGS;
 
 const connectDB = () => {
-    mongoose.connect(CONNECTION_STRINGS,{useUnifiedTopology: true, useNewUrlParser: true });
+    mongoose.connect(process.env.CONNECTION_STRINGS,{useUnifiedTopology: true, useNewUrlParser: true });
 
     //Validating connectionDB
     const connection = mongoose.connection;

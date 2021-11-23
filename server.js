80ede287e32cea4dacf0087d891a4c9e1c559b6b
Jonathan <hackertristania@gmail.com>
2021-11-23 02:11:51 -0600

    fixing minor issues

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use('/api/bookings', require('./routes/bookingsRoute.jsx'))
 
 //DEPLOYMENT
 const path = require('path')
-if(process.env.NODE_ENV === 'production')
+if(process.env.NODE_ENV == "production")
 {
-    app.use('/', express.static('client/build')) //FE Project name deploy path
+    app.use("/", express.static("client/build")) //FE Project name deploy path
 
     app.get("*", (req,res) => {
 
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html')) 
+        res.sendFile(path.resolve(__dirname, "client/build/index.html")) 
     })
 }
 app.get("/", (req,res) => res.send("Hello Dev!"))
